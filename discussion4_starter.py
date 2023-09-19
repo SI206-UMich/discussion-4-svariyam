e0308a85bbf899174ff76892ff48f13595a6ee70
SamVariyam <varis260@newschool.edu>
2023-09-19 13:51:35 -0400

    Finished discussion 4

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -1,51 +1,59 @@
 class Rectangle():
-    # Create the constructor "__init__" method
-    # Arguments: width (an ingeter), height (an integer)
-    # 
-    # It sets an instance variable, "width" to the passed argument, width
-    # It sets an instance variable, "height" to the passed argument, height
+# Create the constructor "__init__" method
+# Arguments: width (an ingeter), height (an integer)
+#
+# It sets an instance variable, "width" to the passed argument, width
+# It sets an instance variable, "height" to the passed argument, height
+# YOUR CODE HERE
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-    # YOUR CODE HERE
+# Create the "__str__" method
+#
+# It returns a string,
+# "A rectangle with width ____ and height ____"
+# YOUR CODE HERE
+    def __str__(self):
+        return "A rectangle with width " + str(self.width) + "and height " + str(self.height)
 
+# Create the "verify_input" method
+#
+# It returns a boolean
+# True if the width and height are positive numbers
+# False otherwise
+# YOUR CODE HERE
 
+    def verify_input(self):
+        if self.width > 0 and self.height > 0:
+            return True
+        else:
+            return False
+        
+# Create the "area" method
+#
+# It first verifies inputs and return "Invalid input" if they are invalid.
+# Otherwise, it returns the area of the rectangle.
+# YOUR CODE HERE
 
-    # Create the "__str__" method
-    #
-    # It returns a string, 
-    #       "A rectangle with width ____ and height ____"
-
-    # YOUR CODE HERE
-
-
-
-    # Create the "verify_input" method
-    #
-    # It returns a boolean
-    #       True if the width and height are positive numbers
-    #       False otherwise
-
-    # YOUR CODE HERE
-
-
-
-    # Create the "area" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the area of the rectangle.
-
-    # YOUR CODE HERE
-
-
-
-    # Create the "perimeter" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the perimeter of the rectangle.
-
-    # YOUR CODE HERE
+    def area(self):
+        if self.verify_input():
+            return self.width * self.height
+        else:
+            return "Invalid input"
     
+# Create the "perimeter" method
+#
+# It first verifies inputs and return "Invalid input" if they are invalid.
+# Otherwise, it returns the perimeter of the rectangle.
+# YOUR CODE HERE
 
-
+    def perimeter(self):
+        if self.verify_input():
+            return 2 * (self.width + self.height)
+        else:
+            return "Invalid input"
+    
 def main():
     r = Rectangle(10, 10)
     print(r)
